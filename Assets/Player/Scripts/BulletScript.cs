cba9f852d1620996fc173f48fb8c4044dff18ae5
Andrew Li <amcli@uwaterloo.ca>
2025-05-22 19:00:58 -0400

    5/22/25 - Optimized Bullet Rotation

--- Assets/Player/Scripts/BulletScript.cs
+++ Assets/Player/Scripts/BulletScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour
@@ -19,8 +20,11 @@ public class BulletScript : MonoBehaviour
         cursorPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
         Vector3 dir = cursorPos - transform.position;
+        Vector3 rot = transform. position - cursorPos;
 
+        float rotation = Mathf.Atan2(rot.y, rot.x) * Mathf.Rad2Deg;
         rb.velocity = new Vector2(dir.x, dir.y).normalized * force;
+        transform.rotation = Quaternion.Euler(0, 0, rotation + 90);
     }
 
     // Update is called once per frame
